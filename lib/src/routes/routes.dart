0743797c1e9b8553cb70bf5621a307c9b8673a71
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-07-19 01:34:30 -0300

    add listView v1.01

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,8 @@
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/listview_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
 import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
@@ -15,6 +17,8 @@ import 'package:componentes/src/pages/home_page.dart';
         'cards':(BuildContext context)=>CardPage(),
         'animatedContainer':(BuildContext context)=>AnimatedContainerPage(),
         'inputs':(BuildContext context)=>InputPage(),
+        'slider':(BuildContext context)=>SliderPage(),
+        'list':(BuildContext context)=>ListaPage(),
 
       };
     }
